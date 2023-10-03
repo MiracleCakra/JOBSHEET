79ee935e752d12e6a6bd085de39f4432a6a8663c
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-03 17:51:21 +0700

    Pertanyaan1 soal no 5

--- jobsheet6/Pemilihan2Percobaan107.java
+++ jobsheet6/Pemilihan2Percobaan107.java
@@ -7,10 +7,15 @@ int tahun = input07.nextInt();
 
 if (tahun % 4 == 0) {
 if ((tahun % 100) != 0) { System.out.println("Tahun Kabisat");
+} else if ((tahun % 100) == 0) { if ((tahun % 400) == 0) {
+System.out.println("Tahun Kabisat");
 } else
 System.out.println("Bukan Tahun Kabisat");
+}
 } else
 System.out.println("Bukan Tahun Kabisat");
+
+input07.close();
 }
 
 }

4e5cdfd7b59cb67dab0f72f0167caf47f4b0aede
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-03 18:24:09 +0700

    Pertanyaan Percobaan2 nomor 1

--- jobsheet6/Pemilihan2Percobaan207.java
+++ jobsheet6/Pemilihan2Percobaan207.java
@@ -1,14 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 public class Pemilihan2Percobaan207 {
 public static void main(String[] args) { Scanner input07 = new Scanner(System.in);
 
-System.out.print("Masukkan sudut pertama segitiga\t : "); float sudut1 = input07.nextFloat(); 
-System.out.print("Masukkan sudut kedua segitiga\t : "); float sudut2 = input07.nextFloat(); 
-System.out.print("Masukkan sudut ketiga segitiga\t : "); float sudut3 = input07.nextFloat();
-float totalSudut = sudut1 + sudut2 + sudut3; if (totalSudut == 180) {
-if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) { System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+System.out.print("Masukkan sudut pertama segitiga\t : "); int sudut1 = input07.nextInt(); 
+System.out.print("Masukkan sudut kedua segitiga\t : "); int sudut2 = input07.nextInt(); 
+System.out.print("Masukkan sudut ketiga segitiga\t : "); int sudut3 = input07.nextInt();
+
+int totalSudut = sudut1 + sudut2 + sudut3;
+if (totalSudut == 180) {
+if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) { 
+System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+} else if ((sudut1 == sudut2) && (sudut2 == sudut3)) {
+System.out.println("Segitiga tersebut adalah Segitiga sama sisi");
+} else if ((sudut1 == sudut2) || (sudut2 == sudut3) || (sudut1
+== sudut3)) {
+System.out.println("Segitiga tersebut adalah Segitiga sama kaki");
 } else
-System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+System.out.println("Segitiga tersebut adalah segitiga sembarang");
 } else
 System.out.println("Bukan Segitiga");
 }

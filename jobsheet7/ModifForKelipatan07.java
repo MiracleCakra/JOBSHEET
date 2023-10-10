5699d00fdc136ff95a78009723f365ca103ac3c9
Cakra Wangsa M.A.W <143616053+MiracleCakra@users.noreply.github.com>
2023-10-10 18:31:25 +0700

    ModifForKelipatan07.java

--- jobsheet7/ModifForKelipatan07.java
+++ jobsheet7/ModifForKelipatan07.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ModifForKelipatan07 {
-    public class void main (String [] args ){
+    public static void main (String [] args ){
         Scanner scan = new Scanner(System.in); 
 
         int kelipatan, total = 0, counter = 0; 

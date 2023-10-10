b23f73735b83d6792ad724c2a7dd855c1e72184d
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-10 17:08:19 +0700

    jobsheet7

--- jobsheet7/ModifForKelipatan07.java
+++ jobsheet7/ModifForKelipatan07.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
- public class ForKelipatan07 {
-    public static void main(String[] args) { 
+public class ModifForKelipatan07 {
+    public class void main (String [] args ){
         Scanner scan = new Scanner(System.in); 
 
         int kelipatan, total = 0, counter = 0; 
+        double rata;
 
         System.out.print("Masukkan bilangan kelipatan (1-9) : "); 
         kelipatan = scan.nextInt(); 
@@ -12,9 +13,11 @@ import java.util.Scanner;
             if (i % kelipatan == 0) { 
                 total += i; 
                 counter++; } } 
-
+                rata = total / counter;
 
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter); 
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total); 
-    }
- }
+        System.out.printf("total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total); 
+        System.out.printf("rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %2f\n", kelipatan, rata);
+
+            }  
+}

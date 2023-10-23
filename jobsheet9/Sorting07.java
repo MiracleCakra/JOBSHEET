c190c6b078e133829aba029eb1276be3366536fe
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-23 21:46:45 +0700

    Tugas

---
+++ jobsheet9/Sorting07.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sorting07 {
+    public static void main(String [] args) {
+       
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Jumlah elemen : ");
+        int jumlahElemen = sc.nextInt();
+
+        int [] nilai = nilai = new int [jumlahElemen];
+
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Masukkan Nilai ke-" + (i + 1) + " : ");
+            nilai[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(nilai);
+
+        int nilaiTertinggi = nilai[jumlahElemen - 1];
+        int nilaiTerendah = nilai[0];
+        double total = 0;
+
+        for (int i = 0; i < jumlahElemen; i++) {
+            total += nilai[i];
+        }
+    
+        double rataRata = total / jumlahElemen;
+
+        System.out.println("Nilai Tertinggi : " + nilaiTertinggi);
+        System.out.println("Nilai Terendah : " + nilaiTerendah);
+        System.out.println("Nilai rata-rata : " + rataRata);
+
+    }
+
+}

c9dc59aefbe4896045d3c847a157eb5da38e48c8
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-23 21:33:33 +0700

    Modifikasi Percobaan 4

--- jobsheet9/BubbleSortExample07.java
+++ jobsheet9/BubbleSortExample07.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class BubbleSortExample07 {
     public static void main(String[] args) {
-        
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int[] intData = {34, 18, 87, 72, 32, 54, 43};
-        int temp;
+        int temp = 0;
 
         for (int i = 0; i < intData.length; i++) {
             for (int j = i + 1; j < intData.length; j++) {
-                if (intData[i] > intData[j]) {
+                if (intData[i] < intData[j]) { 
                     temp = intData[i];
                     intData[i] = intData[j];
                     intData[j] = temp;
                 }
             }
         }
-        
+
         System.out.println("Hasil pengurutan:");
         for (int i = 0; i < intData.length; i++) {
             System.out.println(intData[i]);

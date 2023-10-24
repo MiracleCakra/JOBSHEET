03c4c6eeea0c971cf479f86899eb4ea559f7a49e
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-24 18:33:15 +0700

    Soal nomor 4 Percobaan 2

--- jobsheet9/ArrayNilai07.java
+++ jobsheet9/ArrayNilai07.java
@@ -7,15 +7,15 @@ public class ArrayNilai07 {
         int[] nilaiAkhir = new int[10];
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            System.out.print("Masukkan nilai akhir ke-" + (i + 0) + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-" + i + " lulus " );
-            }else{
-                System.out.println("Mahasiswa ke-" + i + " tidak lulus ");
+                System.out.println("Mahasiswa ke-" + (i + 1) + " lulus ");
+            } else {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " tidak lulus ");
             }
         }
     }

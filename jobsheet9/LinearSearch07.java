ac2ccd2bb6bfce8b76a136ce420af266a1f42371
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-10-25 10:36:55 +0700

    TugasArray07

--- jobsheet9/LinearSearch07.java
+++ jobsheet9/LinearSearch07.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class LinearSearch07 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
 
-        for (int i = 0; i < arrayInt.length; i++){
-            if (arrayInt[i] == key) {
-                hasil = i;
-                break;
-            }
+        Scanner sc = new Scanner(System.in);
+
+        int key;
+        int hasil = 0;
+        
+        System.out.print("Masukkan jumlah elemen array : ");
+        int jml = sc.nextInt();
+        int [] arrayInt = new int[jml];
+        
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke- " +hasil);
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = sc.nextInt();
+         
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                 hasil = i;
+            } 
+        }
+
+        if (arrayInt[hasil] == key) {
+            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan");
+        }
     }
 }

133401430e5f2f98b1edc1ded152b435871c4233
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-11-01 06:51:14 +0700

    Soal nomor 5, Percobaan 1

--- jobsheet10/Bioskop07.java
+++ jobsheet10/Bioskop07.java
@@ -21,15 +21,9 @@ public class Bioskop07 {
    System.out.printf("%s\t%s\n", penonton[2][0], penonton[2][1]);
    System.out.printf("%s\t%s\n", penonton[3][0], penonton[3][1]);
 
-   System.out.println("Penonton pada baris ke-3:");
-   for (int i = 0 ; i < penonton[2].length; i++) {
-     System.out.println(penonton[2][i]);  
-   }
-
-    
    System.out.println(penonton.length);
-   for (String [] barispenonton : penonton) { 
-    System.out.println("Panjang baris: " + barispenonton.length);
+   for (int i = 0 ; i < penonton.length; i++) {
+    System.out.println("Panjang baris ke-" + (i+1) + " adalah " + penonton[i].length);
    }
 
     }

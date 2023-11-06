4387a0aa8351f67bf3487c5848163579d41e7374
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-11-06 22:05:37 +0700

    Percobaan 4, Kuis Tebak Angka

---
+++ jobsheet11/Quiz07.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Quiz07 {
+    public static void main (String[] args){
+        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        char menu = 'y';
+        do{
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+            do{
+                System.out.print("Tebak angka (1-10) : ");
+                int answer = input.nextInt();
+                input.nextLine();
+
+                if (answer < number){
+                    System.out.println("Tebakan anda terlalu rendah");
+                } else if (answer > number){
+                    System.out.println("Tebakan anda terlalu tinggi");
+                } else {
+                    success = true;
+                }
+                
+                success = (answer == number);
+            } while(!success);
+            System.out.print("Apakah anda ingin mengulang permainan (Y/y)? ");
+            menu = input.nextLine().charAt(0);
+
+            } while (menu=='y' || menu=='Y');
+        }
+    }

ab3c8c37d8dcc9507f9fae5590af17d533e259f5
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-11-08 11:11:45 +0700

    TugasIndividu3

--- jobsheet11/TugasIndividu07.java
+++ jobsheet11/TugasIndividu07.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class TugasIndividu07 {
     public static void main (String []args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print ("Masukkan jumlah baris : ");
-        int N = sc.nextInt(); // Jumlah baris
+        System.out.print("Masukkan nilai N (minimal 3) : ");
+        int N = input.nextInt(); // Jumlah baris
 
-        for (int i = N; i >= 1 ; i--) {
+        for (int i = 1; i <= N ; i++) {
             //Membuat spasi 
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for (int j = 1; j <= N; j++) {
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print(N + " ");
+                    
+                } else {
+                   System.out.print("  ");
+                } 
             
             }
             System.out.println();

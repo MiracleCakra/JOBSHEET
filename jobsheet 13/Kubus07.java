66d86166e7c47ab3db38cee45393481869f629a3
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-11-21 06:48:35 +0700

    Kubus

--- jobsheet 13/Kubus07.java
+++ jobsheet 13/Kubus07.java
@@ -1,45 +1,26 @@
+
+
+import java.util.Scanner;
+
 public class Kubus07 {
-    // Variabel sisi sebagai panjang sisi kubus
-    private double sisi;
-
-    // Konstruktor untuk inisialisasi objek Kubus07 dengan panjang sisi tertentu
-    public Kubus07(double sisi) {
-        this.sisi = sisi;
-    }
-
-    // Fungsi untuk menghitung volume kubus
-    public double hitungVolume() {
-        double volume = Math.pow(sisi, 3);
+    static int hitungVolume (int sisi){
+        int volume = sisi*sisi*sisi;
         return volume;
     }
-
-    // Fungsi untuk menghitung luas permukaan kubus
-    public double hitungLuasPermukaan() {
-        double luasPermukaan = 6 * Math.pow(sisi, 2);
-        return luasPermukaan;
+    static int luarPermukaan (int s){
+        int Luar = 6*s*s;
+        return Luar;
     }
-
-    // Getter untuk mendapatkan panjang sisi kubus
-    public double getSisi() {
-        return sisi;
-    }
-
-    // Setter untuk mengubah panjang sisi kubus
-    public void setSisi(double sisi) {
-        this.sisi = sisi;
-    }
-
-    // Main method untuk contoh penggunaan
     public static void main(String[] args) {
-        // Membuat objek Kubus07 dengan panjang sisi 5
-        Kubus07 kubus = new Kubus07(5);
+        Scanner input = new Scanner(System.in);
+        int s ,L , V;
+        System.out.println("Masukkan panjang sisi kubus");
+        s = input.nextInt();
 
-        // Menggunakan objek untuk menghitung volume dan luas permukaan
-        double volume = kubus.hitungVolume();
-        double luasPermukaan = kubus.hitungLuasPermukaan();
+        V = hitungVolume(s);
+        System.out.println("Volume kubus adalah " + V);
 
-        // Menampilkan hasil perhitungan
-        System.out.println("Volume Kubus: " + volume);
-        System.out.println("Luas Permukaan Kubus: " + luasPermukaan);
+        L = luarPermukaan(s);
+        System.out.println("Luas permukaan kubus adalah " + L);
     }
 }

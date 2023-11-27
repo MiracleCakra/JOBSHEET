85b1f12bd2bdb9642a1da66210c86173b5afa9ef
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-11-27 17:14:40 +0700

    Percobaan 1

--- jobsheet 13/UcapanTerimakasih_07.java
+++ jobsheet 13/UcapanTerimakasih_07.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class UcapanTerimakasih_07 {
     public static void UcapanTerimakasih(String nama) {
-        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n"
+        + "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 
     public static void main(String[] args) {
